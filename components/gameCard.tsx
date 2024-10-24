cb4e54f28bb39725260eec23c743d58976b49f50
Danimexivasco <dcano9493@gmail.com>
2024-10-24 16:38:24 +0200

    Enable gameCard link

--- components/gameCard.tsx
+++ components/gameCard.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import {Game} from "../types"
 import Score from "./score"
+import { Link } from 'expo-router'
+import {styled} from "nativewind"
 
 interface GameProps {
   game: Game
 }
 
+const StyledPressable = styled(Pressable)
 const GameCard = ({game}: GameProps) => {
   return (
-    <View key={game.slug} className='flex-row bg-slate-500/10 p-4 rounded-xl gap-4 mb-10'>
-      <Image source={{ uri: game.image }} style={styles.image} />
-      <View>
-        <Text className="mb-1" style={styles.title}>{game.title}</Text>
-        <Score score={game.score} maxScore={100} />
-        <Text className="mt-2 flex-shrink" style={styles.description}>{game.description.slice(0,100)}...</Text>
-      </View>
-    </View>
+    <Link key={game.slug} href={`/${game.slug}`} asChild>
+      <StyledPressable className='active:opacity-70 border border-black active:border-white/50 mb-2 bg-gray-500/10 p-4 rounded-xl '>
+        <View className='flex-row gap-4'>
+          <Image source={{ uri: game.image }} style={styles.image} />
+          <View className='flex-shrink'>
+            <Text className="mb-1" style={styles.title}>{game.title}</Text>
+            <Score score={game.score} maxScore={100} />
+            <Text className="mt-2 flex-shrink" style={styles.description}>{game.description.slice(0,100)}...</Text>
+          </View>
+        </View>
+      </StyledPressable>
+    </Link>
   )
 }
 
